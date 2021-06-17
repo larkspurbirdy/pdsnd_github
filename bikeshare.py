5dfa1f9c61922b11d36c22da802d1fa3abf7e501
larkspurbirdy <larkspurbirdy@gmail.com>
2021-06-16 22:09:41 -0400

    refactor: Add exit message
    Added "Have a nice day!" message to be displayed when the user exits the
    program.

--- bikeshare.py
+++ bikeshare.py
@@ -191,6 +191,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Have a nice day!")
             break
 
 
